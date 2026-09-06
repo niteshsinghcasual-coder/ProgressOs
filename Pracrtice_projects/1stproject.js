f08c70b4a9c60681b21e4d6840a78dba56b322d5
niteshsinghcasual-coder <niteshsinghcasual@gmail.com>
2026-09-06 09:26:50 +0530

    This is my project

--- Pracrtice_projects/1stproject.js
+++ Pracrtice_projects/1stproject.js
@@ -1,10 +1,25 @@
 const buttons=document.querySelectorAll(".botton");
 const body=document.querySelector("body");
 
-buttons.forEach(function(button)
+buttons.forEach(function (button)
 {
+    console.log(button)
     button.addEventListener('click',function(e){
-        console.log(e)
-        console.log(e.target)
+       if(e.target.id=== 'grey')
+       {
+        body.style.backgroundColor=e.target.id;
+       }
+       else if(e.target.id==="purple")
+       {
+        body.style.backgroundColor=e.target.id;
+       }
+       else if(e.target.id === "blue")
+       {
+        body.style.backgroundColor= e.target.id;
+       }
+       else if(e.target.id === "yellow")
+       {
+        body.style.backgroundColor=e.target.id;
+       }
     });
 })
